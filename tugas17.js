3ba7ccea0f3d81263118a5634d77c7bc7b8b3d5a
Simon Fredy Sinaga <simonfredysinaga@gmail.com>
2020-03-24 20:41:06 +0700

    Tugas 17 JS

---
+++ tugas17.js
@@ -0,0 +1,15 @@
+function tugasTujuhBelas() {
+    // Tugas 1 
+    var kSatu = "Saya beLajar bahaSa peMrograman deNgan khUsuk";
+    console.log(kSatu.toLowerCase());
+    console.log(kSatu.toUpperCase());
+
+    var kDua = "Allen SedAng MemAncinG IkaN di SuNgai, DIA MEndapat IkAN GaBuS 3 Ekor dan LanGsuNg Dijual";
+    console.log(kDua.toLowerCase());
+    console.log(kDua.toUpperCase());
+
+    // Tugas 2
+    var lIpsum = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum."
+    console.log(lIpsum.charCodeAt(0), '-', lIpsum.charCodeAt(1), '-', lIpsum.charCodeAt(2), '-', lIpsum.charCodeAt(3), '-', lIpsum.charCodeAt(4), '-', lIpsum.charCodeAt(5));
+}
+tugasTujuhBelas();
